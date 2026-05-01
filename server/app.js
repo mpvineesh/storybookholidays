29740fa09a19d2c84b1b412e7474ba62f75eb5d5
Vineesh M P <vneesh@gmail.com>
2026-05-01 15:56:39 +0530

    Changed request size

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ app.use(
     },
   })
 );
-app.use(express.json({ limit: "2mb" }));
+app.use(express.json({ limit: "5mb" }));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.get("/api/health", (_req, res) => {
